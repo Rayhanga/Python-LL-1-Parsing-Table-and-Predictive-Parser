048a404607d06787b97f54e48e3bb8e2aa20c0f4
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2022-01-21 20:48:00 +0700

    Add tugas2.py (predictive parser)

--- readme.md
+++ readme.md
@@ -39,3 +39,80 @@ T=>FT'
 T'=>*FT'|Є
 F=>(E)|id
 ```
+
+# Python Predictive Parser (Tugas 2)
+
+## Input file format
+### `input_token`
+```
+TOTAL NUMBER OF TOKENS (N)
+TOKEN 1
+.
+.
+TOKEN N
+```
+
+Example
+```
+2
+id + id * id $
+id + * id id $
+```
+
+### `output_table` (from Tugas 1)
+```
+ROW
+COL
+NON TERMINAL SYMBOL 1
+TERMINAL SYMBOL 1 | PRODUCTION RULE
+.
+.
+TERMINAL SYMBOL COL | PRODUCTION RULE
+NON TERMINAL SYMBOL 2
+.
+.
+.
+NON TERMINAL SYMBOL ROW
+```
+
+Example
+
+```
+5
+6
+E
+(|E   => TE'     
+id|E   => TE'     
++|ERROR          
+*|ERROR          
+)|ERROR          
+$|ERROR          
+E'
++|E'  => +TE'    
+$|E'  => Є       
+)|E'  => Є       
+id|ERROR          
+*|ERROR          
+(|ERROR          
+T
+(|T   => FT'     
+id|T   => FT'     
++|ERROR          
+*|ERROR          
+)|ERROR          
+$|ERROR          
+T'
+*|T'  => *FT'    
+$|T'  => Є       
++|T'  => Є       
+)|T'  => Є       
+id|ERROR          
+(|ERROR          
+F
+(|F   => (E)     
+id|F   => id      
++|ERROR          
+*|ERROR          
+)|ERROR          
+$|ERROR          
+```
---
+++ tugas2.py
@@ -0,0 +1,82 @@
+import tugas1
+
+def read_parsing_table(table, input_file):
+  row = int(input_file.readline().replace("\n", ""))
+  col = int(input_file.readline().replace("\n", ""))
+  for _ in range(row):
+    non_terminal = input_file.readline().replace("\n", "")
+    table[non_terminal] = {}
+    for _ in range(col):
+      [terminal, value] = input_file.readline().replace("\n", "").split("|")
+      table[non_terminal][terminal] = value
+
+def get_symbols_from_parsing_table(table):
+  non_terminal = list(table.keys())
+  terminal = list(table[next(iter(table))].keys())
+  
+  return non_terminal, terminal
+
+def raise_custom_error():
+  global stack, output, token
+
+  stack = []
+  token = []
+  output = "ERROR"
+  print(f'{"".join(stack):20} {"".join(token):20} {output:20}')
+
+def parse(parsing_table, token, stack, output, non_terminal, terminal):
+  print(f'{"".join(stack):20} {"".join(token):20} {output:20}')
+  # Rule 1
+  if stack[-1] == token[0] == "$":
+    return
+  # Rule 2
+  if stack[-1] == token[0] != "$":
+    output = ""
+    del token[0]
+    del stack[-1]
+    print(f'{"".join(stack):20} {"".join(token):20} {output:20}')
+  # Rule 3
+  if stack[-1] in terminal:
+    if stack[-1] != token[0]:
+      # # Python's Error Handler
+      # raise Exception("Syntax Error")
+      # Custom Error Handler
+      raise_custom_error()
+      return
+  # Rule 4
+  if stack[-1] in non_terminal:
+    product = parsing_table[stack[-1]][token[0]]
+    output = product
+    if product.strip() == "ERROR":
+      # # Python's Error Handler
+      # raise Exception("Parsing error")
+      # Custom Error Handler
+      raise_custom_error()
+      return
+    del stack[-1]
+    if "Є" not in product:
+      temp = [symbol for symbol in tugas1.tokenize(product.split("=>")[-1], non_terminal, terminal)[0]]
+      temp.reverse()
+      stack += temp
+  parse(parsing_table, token, stack, output, non_terminal, terminal)
+
+def main():
+  print("\nTUGAS 2")
+  parsing_table = {}
+  with open("output_table", encoding="utf-8") as table_file:
+    read_parsing_table(parsing_table, table_file)
+  
+  with open("input_token", encoding="utf-8") as token_file:
+    total_token = int(token_file.readline().replace("\n", ""))
+    for _ in range(total_token):
+      raw_token = token_file.readline().replace("\n", "")
+      stack = ["$", next(iter(parsing_table))]
+      non_terminal, terminal = get_symbols_from_parsing_table(parsing_table)
+      token = tugas1.tokenize(raw_token, non_terminal, terminal)[0]
+      output= ""
+      print(f'\nToken: {raw_token}')
+      print(f'{"Stack":20} {"Input":20} {"Output":20}')
+      parse(parsing_table, token, stack, output, non_terminal, terminal)
+
+if __name__ == "__main__":
+  main()
